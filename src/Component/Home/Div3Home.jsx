7b3f5d149310533b0fbcc688027e75fe25f92fe0
Pankaj_Aru <85275731+Pankaj-Aru@users.noreply.github.com>
2022-04-11 10:57:48 +0530

    changes in image

--- src/Component/Home/Div3Home.jsx
+++ src/Component/Home/Div3Home.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import Button from "../Buttons"
 import Heading from "../Heading.jsx"
 import Img1 from "../../Assets/Images/Courses/img-1.jpg"
-import Img2 from "../../Assets/Images/Courses/img-2.jpg"
-import Img3 from "../../Assets/Images/Courses/img-3.jpg"
-import Img4 from "../../Assets/Images/Courses/img-4.jpg"
-import Img5 from "../../Assets/Images/Courses/img-5.jpg"
-import Img6 from "../../Assets/Images/Courses/img-6.jpg"
-import Img7 from "../../Assets/Images/Courses/img-7.jpg"
-import Img8 from "../../Assets/Images/Courses/img-8.jpg"
-import Img0 from "../../Assets/Images/Courses/img-0.jpg"
+
 import Prof from "../../Assets/Images/profile/Prof.png"
 
 import "../../Assets/Css/Home/Div3Home.css";
@@ -17,7 +10,7 @@ import data from "../../Assets/Data/HomeSlider.json";
 
 export default function Div3Home() {
 
-const Image=[Img0,Img1,Img2,Img3,Img4,Img5,Img6,Img7,Img8]
+const Image=[Img1,Img1,Img1,Img1,Img1,Img1,Img1,Img1,Img1]
 
   return (
     
