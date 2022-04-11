35405302514af33245af48a12658a5323fa9dd4e
Pankaj_Aru <85275731+Pankaj-Aru@users.noreply.github.com>
2022-04-11 10:58:45 +0530

    Changes in Image

--- src/Component/Home/Div5Home.jsx
+++ src/Component/Home/Div5Home.jsx
@@ -3,13 +3,11 @@ import data from "../../Assets/Data/HomeSlider.json";
 
 import "../../Assets/Css/Home/Div5Home.css";
 import Img1 from "../../Assets/Images/Followers/img-1.svg"
-import Img2 from "../../Assets/Images/Followers/img-2.svg"
-import Img3 from "../../Assets/Images/Followers/img-3.svg"
-import Img0 from "../../Assets/Images/Followers/img-0.svg"
+
 
 export default function Div5Home() {
 
-const Image=[Img0,Img1,Img2,Img3]
+const Image=[Img1,Img1,Img1,Img1]
 
   
   return (
